a745510239cfea9af9ed50b2455a078a3149965d
s3780973 <s3780973@student.rmit.edu.au>
2019-08-29 20:16:47 +1000

    Added half done prices section and synopsis

--- a2/index.php
+++ a2/index.php
@@ -43,32 +43,87 @@
 
             <!-- About Us -->
             <section id="about_us">
-            <header class="topic">
+                <header class="topic">
                     <h2>About Us</h2>
-                    <p>Welcome to the Lunardo Cinema! <br> We are very excited to announce our brand new Cinema after extensive improvements and renovations. From upgrading our seating to comfy leather recliners,
+                    <p>Welcome to the Lunardo Cinema!<br> We are very excited to announce our brand new Cinema after extensive improvements and renovations. From upgrading our seating to comfy leather recliners,
                     to overhauling our projector and sound systems, we hope to bring an all new movie experience for you, your friends and family!</p>
                     <div>
-                        <p>Our new standard seats have been made a lot softer and much more comfier, perfect for those lengthy films!</p>
+                        <p class="title">Standard Seating</p>
+                        <p class = "desc">Our new standard seats have been made a lot softer and much more comfier, perfect for those lengthy films!</p>
                     <img src='../../media/standard-seat.png'/>
                     </div>
                     <div>
-                        <p>Our first class seats can now recline back and include a food tray, no need to avoid the front rows anymore!</p>
+                        <p class="title">First Class Seating</p>
+                        <p>Our first class seats can now recline back and include a food tray, no need to fear the front rows anymore!</p>
                         <img src='../../media/first-class-seat.png'/>
                     </div>
                     <div>
+                        <p class="title">Projectors</p>
                         <p>With our brand new Dolby 3D projectors, you can choose to now watch your favourite movies in 3D!</p>
                         <img src='../../media/projector.png'/>
                     </div>
                     <div>
+                        <p class="title">Sound System</p>
                         <p>Our brand new Dolby sound system now allows sound to come from all over, giving an immersive experience!</p>
                         <img src='../../media/sound-system.png'/>
                     </div>
            </header>
            </section>
-            <!-- Prices -->
+            
+           <!-- Prices -->
             <section id="prices">
                 <header class="topic">
                     <h2>Prices</h2>
+                    <div>
+                    <h2>Standard Seating</h2>
+                    <table>
+  <tr>
+    <th>Seat</th>
+    <th>Mondays, Wednesdays and Weekdays 12PM</th>
+    <th>Regular Price</th>
+  </tr>
+  <tr>
+    <td>Adult</td>
+    <td>$14.00</td>
+    <td>$19.80</td>
+  </tr>
+  <tr>
+    <td>Concession</td>
+    <td>$12.50</td>
+    <td>$17.50</td>
+  </tr>
+  <tr>
+    <td>Child</td>
+    <td>$11.00</td>
+    <td>$15.30</td>
+  </tr>
+</table>
+</div>
+<div>
+            <h2>First Class Seating</h2>        
+            <table>
+  <tr>
+    <th>Seat</th>
+    <th>Mondays, Wednesdays and Weekdays 12PM</th>
+    <th>Regular Price</th>
+  </tr>
+  <tr>
+    <td>Adult</td>
+    <td>$24.00</td>
+    <td>$30.00</td>
+  </tr>
+  <tr>
+    <td>Concession</td>
+    <td>$22.50</td>
+    <td>$27.00</td>
+  </tr>
+  <tr>
+    <td>Child</td>
+    <td>$21.00</td>
+    <td>$24.00</td>
+  </tr>
+</table>
+                    </div>
                 </header>
             </section>
 
@@ -76,27 +131,45 @@
             <section id="now_showing">
                 <header class="topic">
                     <h2>Now Showing</h2>
-                    <p>We are currently screening the following movies all weekdays and weekends:</p>
-                    <div>
+                    <p>We are currently screening the following movies all weekdays and weekends</p>
+                    <div class="movie">
                     <img src='../../media/avengers-endgame.png'/>
                         <p class = "title">Avengers: Engame   M</p>
-                        <p class = "times">Times:<br>Wednesday - 9PM<br>Thursday - 9PM<br>Friday - 9PM<br>Saturday - 6PM<br>Sunday - 7PM</p>
-                    </div>
-                    <div>
+                        <p class = "times">Times:<br>Wednesday - 9PM<br>Thursday - 9PM<br>Friday - 9PM<br>Saturday - 6PM<br>Sunday - 6PM</p>
+                   </div>
+                    <div class="movie">
                         <img src='../../media/top-end-wedding.png'/>
                         <p class = "title">Top End Wedding   M</p>
                         <p class = "times">Times:<br>Monday - 6PM<br>Tuesday - 6PM<br>Saturday - 3PM<br>Sunday - 3PM</p>
                     </div>
-                    <div>
+                    <div class="movie">
                         <img src='../../media/dumbo.png'/>
                         <p class = "title">Dumbo   PG</p>
                         <p class = "times">Times:<br>Monday - 12PM<br>Tuesday - 12PM<br>Wednesday - 6PM<br>Thursday - 6PM<br>Friday - 6PM<br>Saturday 12PM<br>Sunday - 12PM</p>
                     </div>
-                    <div>
+                    <div class="movie">
                         <img src='../../media/the-happy-prince.png'/>
                         <p class = "title">The Happy Prince   MA</p>
                         <p class = "times">Times:<br>Wednesday - 12PM<br>Thursday - 12PM<br>Friday - 12PM<br>Saturday 9PM<br>Sunday - 9PM</p>
-                    </div>
+                    </div class="synopsis">
+                    <article>
+                        <h2>The Avengers: Endgame   (M)</h2>
+                        <video controls>
+                        <source src="../../media/avengers-trailer.mp4" type="video/mp4">
+                        <source src="../../media/avengers-trailer.ogg" type="video/ogg">
+                        Your browser does not support the video tag.
+                        </video>
+                        <div class="synopsis">
+                        <p class="title">Plot Description:</p>
+                        <p>Adrift in space with no food or water, Tony Stark sends a message to Pepper Potts as his oxygen supply starts to dwindle. Meanwhile, the remaining Avengers -- Thor, Black Widow, Captain America and Bruce Banner -- must figure out a way to bring back their vanquished allies for an epic showdown with Thanos -- the evil demigod who decimated the planet and the universe.</p>
+                        </div>
+                        <span class="title">Make a booking:</span>
+                        <button type="button">Wednesday - 9PM</button>
+                        <button type="button">Thursday - 9PM</button>
+                        <button type="button">Friday - 9PM</button>
+                        <button type="button">Saturday - 6PM</button>
+                        <button type="button">Sunday - 6PM</button>
+                    </article>
                 </header>
             </section>
 
@@ -106,7 +179,7 @@
         <footer>
             <div>&copy;<script>
                 document.write(new Date().getFullYear());
-                </script> Put your name(s), student number(s) and group name here. Last modified
+                </script> John Patrikios (s3780973) | No Group. Last modified
                 <?= date ("Y F d  H:i", filemtime($_SERVER['SCRIPT_FILENAME'])); ?>.</div>
             <div>Disclaimer: This website is not a real website and is being developed as part of a School of Science
                 Web Programming course at RMIT University in Melbourne, Australia.</div>
